eec6d7017e3f0db80bbc07a091329f5488b02872
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-08 16:35:22 -0300

    Req 1, implementa teste para inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,29 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    id = "450"
+    product_name = "Product 1"
+    company_name = "Company Name"
+    manufacturing_date = "2022-01-01"
+    expiration_date = "2023-01-01"
+    serial_number = "ABC123"
+    storage_instructions = "Store in a cool, dry place."
+
+    product = Product(
+        id,
+        product_name,
+        company_name,
+        manufacturing_date,
+        expiration_date,
+        serial_number,
+        storage_instructions,
+    )
+
+    assert product.id == id
+    assert product.product_name == product_name
+    assert product.company_name == company_name
+    assert product.manufacturing_date == manufacturing_date
+    assert product.expiration_date == expiration_date
+    assert product.serial_number == serial_number
+    assert product.storage_instructions == storage_instructions
