cba84c39e0fb2daad346ec237dd62a0e04a3d097
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-08 18:36:35 -0300

    Req 1, retorna com o código anterior

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,9 +1,8 @@
 from inventory_report.product import Product
-import pytest
 
 
-@pytest.fixture
-def sample_product():
+def test_create_product() -> None:
+
     id = "450"
     product_name = "Product 1"
     company_name = "Company Name"
@@ -12,7 +11,7 @@ def sample_product():
     serial_number = "ABC123"
     storage_instructions = "Store in a cool, dry place."
 
-    return Product(
+    product = Product(
         id,
         product_name,
         company_name,
@@ -22,40 +21,10 @@ def sample_product():
         storage_instructions,
     )
 
-
-def test_create_product(sample_product) -> None:
-    product = sample_product
-
-    assert product.id == "450"
-    assert product.product_name == "Product 1"
-    assert product.company_name == "Company Name"
-    assert product.manufacturing_date == "2022-01-01"
-    assert product.expiration_date == "2023-01-01"
-    assert product.serial_number == "ABC123"
-    assert product.storage_instructions == "Store in a cool, dry place."
-
-    # id = "450"
-    # product_name = "Product 1"
-    # company_name = "Company Name"
-    # manufacturing_date = "2022-01-01"
-    # expiration_date = "2023-01-01"
-    # serial_number = "ABC123"
-    # storage_instructions = "Store in a cool, dry place."
-
-    # product = Product(
-    #     id,
-    #     product_name,
-    #     company_name,
-    #     manufacturing_date,
-    #     expiration_date,
-    #     serial_number,
-    #     storage_instructions,
-    # )
-
-    # assert product.id == id
-    # assert product.product_name == product_name
-    # assert product.company_name == company_name
-    # assert product.manufacturing_date == manufacturing_date
-    # assert product.expiration_date == expiration_date
-    # assert product.serial_number == serial_number
-    # assert product.storage_instructions == storage_instructions
+    assert product.id == id
+    assert product.product_name == product_name
+    assert product.company_name == company_name
+    assert product.manufacturing_date == manufacturing_date
+    assert product.expiration_date == expiration_date
+    assert product.serial_number == serial_number
+    assert product.storage_instructions == storage_instructions
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,15 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report import Product
+
+
+def test_product_report(sample_product) -> None:
+    product = sample_product
+
+    expected_reported_phrase = (
+        "The product Product 1, with serial number ABC123, "
+        "manufactured on 2022-01-01 by the company Company Name, "
+        "valid until 2023-01-01, "
+        "must be stored according to the following instructions: "
+        "Store in a cool, dry place."
+    )
+
+    assert str(product) == expected_reported_phrase
