e1d7cc8472d427a22fd58b2b76ecbb13b9708886
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-08 19:29:14 -0300

    Req 2, testa relatório individual gerado por produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,15 +1,32 @@
-from inventory_report import Product
+from inventory_report.product import Product
 
 
-def test_product_report(sample_product) -> None:
-    product = sample_product
+def test_product_report() -> None:
+    id = "450"
+    product_name = "Product 1"
+    company_name = "Company Name"
+    manufacturing_date = "2022-01-01"
+    expiration_date = "2023-01-01"
+    serial_number = "ABC123"
+    storage_instructions = "Store in a cool, dry place."
 
-    expected_reported_phrase = (
-        "The product Product 1, with serial number ABC123, "
-        "manufactured on 2022-01-01 by the company Company Name, "
-        "valid until 2023-01-01, "
-        "must be stored according to the following instructions: "
-        "Store in a cool, dry place."
+    product = Product(
+        id=id,
+        product_name=product_name,
+        company_name=company_name,
+        manufacturing_date=manufacturing_date,
+        expiration_date=expiration_date,
+        serial_number=serial_number,
+        storage_instructions=storage_instructions,
     )
 
-    assert str(product) == expected_reported_phrase
+    expected_report = (
+        f"The product {id} - {product_name} "
+        f"with serial number {serial_number} "
+        f"manufactured on {manufacturing_date} "
+        f"by the company {company_name} "
+        f"valid until {expiration_date} "
+        f"must be stored according to the following instructions: "
+        f"{storage_instructions}."
+    )
+    assert str(product) == expected_report
