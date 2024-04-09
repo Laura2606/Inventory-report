4e3a2d626f93e88b5368a2bd474dfdbd10f179e0
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-09 16:36:10 -0300

    Req 6, iniciando

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,12 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from abc import ABC, abstractmethod
+
+
+class Report(ABC):
+    @abstractmethod
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    @abstractmethod
+    def generate(self) -> str:
+        pass
